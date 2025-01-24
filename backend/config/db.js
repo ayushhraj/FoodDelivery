a30c14824a949bc50ca15c289677bb4ad1998320
ayushhraj <115882236+ayushhraj@users.noreply.github.com>
2025-01-25 04:27:41 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://ayushraj:kiit123@cluster0.drtfu.mongodb.net/fooddelivery').then(() =>
-        console.log('DB Connected'));
-    }
+    await mongoose.connect(process.env.MONGODB_URI).then(() => {
+        console.log('DB Connected');
+    })
