9ed0064be74683dcc2c100c7a221395bde21d131
ayushhraj <115882236+ayushhraj@users.noreply.github.com>
2025-01-30 04:07:18 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,4 +4,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI).then(() => {
         console.log('DB Connected');
-    })
+    }).catch((error) => {
+        console.error('DB Connection Error:', error);
+    });
+};
